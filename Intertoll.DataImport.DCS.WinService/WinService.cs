7faac23c4125530fa11ae6211a3874b5ae857cb7
Sthabiso Jaffar <sthabiso.jaffar@iotnxt.com>
2018-12-14 10:23:48 +0200

    latest

---
+++ Intertoll.DataImport.DCS.WinService/WinService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace Intertoll.DataImport.DCS.WinService
+{
+    public partial class WinService : ServiceBase
+    {
+        public WinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
